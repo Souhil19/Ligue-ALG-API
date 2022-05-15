764fbe67bb3dbdb3541a736b1e6a2b461b134d19
Souhil19 <s.omari@esi-sba.dz>
2022-05-15 21:25:19 +0100

    deploy

--- apitest/settings.py
+++ apitest/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*7ned@x423t6lm-y&u&!iqkc-^1h-w=)be52a2dfg3#xkjm4gc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','souhil19.pythonanywhere.com']
 
 # Actual directory user files go to
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'apitest/mediafiles')
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
